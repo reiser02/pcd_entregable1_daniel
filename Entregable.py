f85b09d77347d076143108c49c57544cd5afeb4c
reiser02 <reiser976@outlook.es>
2024-04-07 03:30:47 +0200

    Modificación de la estructura de datos interna de universidad, Profesor y Estudiante

--- Entregable.py
+++ Entregable.py
@@ -11,22 +11,35 @@ class Persona:
         self.dni = dni
 
 class Profesor(Persona):
-    #Asignaturas es un conjunto para no permitir que hayan repetidas
     def __init__(self, nombre, dni, departamento):
         super().__init__(nombre, dni)
         self.creditos = 0
         self.departamento = departamento
         self.asignaturas = list()
 
-    def add_asignatura(self, asignatura):
-        for e in self.asignaturas:
-            if asignatura is e:
-                return
+    #Añade una asignatura a la lista de asignaturas si no está ya y si no se excede el máximo de créditos
+    def add_asignatura(self, codAsignatura, creditos):
+        if codAsignatura in self.asignaturas:
+            return
             
-        if self.creditos + asignatura.creditos > 72:
+        if self.creditos + creditos > 72:
             raise ValueError("Se excede el máximo de créditos que un profesor puede impartir")
-        self.creditos += asignatura.creditos
-        self.asignaturas.append(asignatura)
+        self.creditos += creditos
+        self.asignaturas.append(codAsignatura)
+
+    '''
+    Ya que el enunciado dice que se tiene que poder eliminar a un profesor de un departamento,
+    al cambiarlo se le estaría borrando se su anterior departamento.
+    '''
+    def cambiar_departamento(self, departamento):
+        '''
+        Así se obliga a que sea instancia de EDepartamento y no hay problemas con que se ponga None y se quede
+        sin departamento
+        '''
+        if not isinstance(departamento, EDepartamento):
+            raise TypeError("Departamento no pertece a la clase EDepartamento")
+        
+        self.departamento = departamento
 
 class Titular(Profesor):
     def __init__(self, nombre, dni, departamento, areaInvestivacion):
@@ -42,9 +55,8 @@ class Titular(Profesor):
         print(f"Departamento: {self.departamento}\n")
         print(f"Area de investigación: {self.areaInvestivacion}\n")
         print(f"Creditos: {self.creditos}\n")
-        print(f"Asignaturas: ")
-        for a in self.asignaturas:
-            print(a.codigo)
+        print(f"Asignaturas: {self.asignaturas}")
+        return "\n"
 
 class Asociado(Profesor):
     def __init__(self, nombre, dni, departamento, trabajoExterno):
@@ -57,9 +69,8 @@ class Asociado(Profesor):
         print(f"Departamento: {self.departamento}\n")
         print(f"Trabajo externo: {self.trabajoExterno}\n")
         print(f"Creditos: {self.creditos}\n")
-        print(f"Asignaturas: ")
-        for a in self.asignaturas:
-            print(a.codigo)
+        print(f"Asignaturas: {self.asignaturas}")
+        return "\n"
 
 class Asignatura():
     def __init__(self, codigo, creditos, curso):
@@ -72,94 +83,168 @@ class Asignatura():
         print(f"Codigo: {self.codigo}\n")
         print(f"Creditos: {self.creditos}\n")
         print(f"Asignaturas: {cursos[self.curso]}\n")
+        return "\n"
 
 class Estudiante(Persona):
     def __init__(self, nombre, dni, curso):
         super().__init__(nombre, dni)
         self.curso = curso
-        self.asignaturas = list()
+        self.asignaturasMatriculadas = list()
+        self.asignaturasAprobadas = list()
 
-    def add_asignatura(self, asignatura):
-        for e in self.asignaturas:
-            if asignatura is e:
-                return
+    def add_asignatura(self, codAsignatura):
+        '''
+        Si el código está en una de las dos listas, entonces la asignatura no se tiene que añadir.
+        '''
+        if codAsignatura in self.asignaturasMatriculadas or codAsignatura in self.asignaturasAprobadas:
+            return
         
-        self.asignaturas.append(asignatura)
+        self.asignaturasMatriculadas.append(codAsignatura)
+
+    def aprobar_asignatura(self, codAsignatura):
+        '''
+        Como list().remove tiene una excepción si no el existe el valor, lo meto en un try except.
+        '''
+        try:
+            self.asignaturasMatriculadas.remove(codAsignatura)
+            self.asignaturasAprobadas.append(codAsignatura)
+        except Exception as e:
+            print("Excepción: ", e)
 
     def __str__(self):
         cursos = ["Primero", "Segundo", "Tercero", "Cuarto"]
         print(f"Nombre: {self.nombre}\n")
         print(f"DNI: {self.dni}\n")
         print(f"Curso: {cursos[self.curso]}\n")
-        print(f"Asignaturas: ")
-        for a in self.asignaturas:
-            print(a.codigo)
+        print(f"Asignaturas matriculadas: {self.asignaturasMatriculadas}")
+        print(f"Asignaturas aprobadas: {self.asignaturasAprobadas}")
+        return "\n"
 
 
 class Universidad():
     def __init__(self):
-        self.listaProfesores = list()
-        self.listaAlumnos = list()
-        self.listaAsignaturas = list()
+        '''
+        He optado por el uso de diccionarios indexados por dni o código de asignatura en lugar de listas
+        para evitar recorrer las listas cada vez que sea necesario buscar a un profesor, alumno o asignatura.
+        '''
+        self.dictProfesores = dict()
+        self.dictAlumnos = dict()
+        self.dictAsignaturas = dict()
 
     def add_profesor(self, nombre, dni, titular, departamento, areaInv=None, trabajoExterno=None):
         if not isinstance(departamento, EDepartamento):
             raise TypeError("Departamento no pertece a la clase EDepartamento")
         
-        for p in self.listaProfesores:
-            if dni == p.dni:
-                return 
+        #Así hago que acabe el método en caso de que ya exista un profesor con ese dni
+        if dni in self.dictProfesores:
+            return
         
+        #Comprobación de los requisitos que se imponen y excepciones en caso de no cumplirse
         if titular:
             if areaInv is None:
                 raise ValueError("Si el profesor es titular, debe tener area de investigación")
             elif trabajoExterno is not None:
                 raise ValueError("Si el profesor es titular, no debe tener un trabajo externo")
-            self.listaProfesores.append(Titular(nombre, dni, departamento, areaInv))
+            self.dictProfesores[dni] = Titular(nombre, dni, departamento, areaInv)
         else:
-            self.listaProfesores.append(Asociado(nombre, dni, departamento, trabajoExterno))
+            if areaInv is not None:
+                raise ValueError("Si el profesor es asociado, no debe tener area de investigación")
+            elif trabajoExterno is None:
+                raise ValueError("Si el profesor es asociado, debe tener un trabajo externo")
+            self.dictProfesores[dni] = Asociado(nombre, dni, departamento, trabajoExterno)
 
     def add_alumno(self, nombre, dni, curso):
-        for a in self.listaAlumnos:
-            if dni == a.dni:
-                return
+        #Si ya existe un alumno con ese dni, acaba la función
+        if dni in self.dictAlumnos:
+            return
         
-        self.listaAlumnos.append(Estudiante(nombre, dni, curso))
+        self.dictAlumnos[dni] = Estudiante(nombre, dni, curso)
 
     def add_asignatura(self, codigo, creditos, curso):
-        for a in self.listaAsignaturas:
-            if codigo == a.codigo:
-                return
+        #Si ya existe una asignatura con ese código, acaba la función
+        if codigo in self.dictProfesores:
+            return
             
-        self.listaAsignaturas.append(Asignatura(codigo, creditos, curso))
+        self.dictAsignaturas[codigo] = Asignatura(codigo, creditos, curso)
 
     def add_asignatura_profesor(self, dni, codigo):
-        for p in self.listaProfesores:
-            if dni == p.dni:
-                for a in self.listaAsignaturas:
-                    if codigo == a.codigo:
-                        p.add_asignatura(a)
-                        break
+        #Controlada la excepción en caso de que la clave por la que se indexa no exista.
+        try:
+            profesor = self.dictProfesores[dni]
+        except Exception as e:
+            print(f"El profesor con dni: {dni} no existe", e)
+
+        try:
+            asignatura = self.dictAsignaturas[codigo]
+        except Exception as e:
+            print(f"La asigntura con codigo: {codigo} no existe", e)
+
+        profesor.add_asignatura(codigo, asignatura.creditos)
 
     def add_asignatura_alumno(self, dni, codigo):
-        for a in self.listaAlumnos:
-            if dni == a.dni:
-                for asig in self.listaAsignaturas:
-                    if codigo == asig.codigo:
-                        a.add_asignatura(asig)
-                        break
+        #Controlada la excepción en caso de que la clave por la que se indexa no exista.
+        try:
+            alumno = self.dictAlumnos[dni]
+        except Exception as e:
+            print(f"El alumno con dni: {dni} no existe", e)
+
+        try:
+            asignatura = self.dictAsignaturas[codigo]
+        except Exception as e:
+            print(f"La asigntura con codigo: {codigo} no existe", e)
+
+        alumno.add_asignatura(codigo, asignatura.creditos)
+
+    def eliminar_alumno(self, dni):
+        try:
+            self.dictAlumnos.pop(dni)
+        except Exception as e:
+            print(f"No existe el alumno con dni: {dni}", e)
+
+    def cambiar_departamento(self, dni, departamento):
+        #Controlada la excepción en caso de que la clave por la que se indexa no exista.
+        try:
+            profesor = self.dictProfesores[dni]
+        except Exception as e:
+            print(f"El profesor con dni: {dni} no existe", e)
+
+        profesor.cambiar_departamento(departamento)
+
 
     def obtener_profesor(self, dni):
-        for p in self.listaProfesores:
-            if dni == p.dni:
-                return p
+        try:
+            return self.dictProfesores[dni]
+        except Exception as e:
+            print(f"El profesor con dni: {dni} no existe", e)
 
     def obtener_alumno(self, dni):
-        for a in self.listaAlumnos:
-            if dni == a.dni:
-                return a
+        try:
+            return self.dictAlumnos[dni]
+        except Exception as e:
+            print(f"El alumno con dni: {dni} no existe", e)
 
     def obtener_asignatura(self, codigo):
-        for a in self.listaAsignaturas:
-            if codigo == a.codigo:
-                return a
+        try:
+            return self.dictAsignaturas[codigo]
+        except Exception as e:
+            print(f"La asigntura con codigo: {codigo} no existe", e)
+
+    def __str__(self):
+        print(f"dictProfesores: {self.dictProfesores} \n")
+        print(f"dictAlumnos: {self.dictAlumnos} \n")
+        print(f"dictAsignaturas: {self.dictAsignaturas} \n")
+        return "\n"
+
+
+if __name__ == "__main__":
+    titular = Titular("Jose", "1234", EDepartamento.DIIC, "software")
+    asociado = Asociado("Hernesto", "1235", EDepartamento.DITEC, "Programador")
+    alumno = Estudiante("Carlos", "1236", 2)
+    asignatura = Asignatura("3124", 6, 1)
+    universidad = Universidad()
+
+    print(titular)
+    print(asociado)
+    print(alumno)
+    print(asignatura)
+    print(universidad)
